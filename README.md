b7ce98fb6a0bfac913a2012beacd91bef938b0f2
sultanularefin <sultanul.arefin@softbakso.com>
2019-02-24 19:23:52 +0600

    1st

---
+++ README.md
@@ -0,0 +1,9 @@
+# Snackbar Example
+
+## Usage
+
+1. `(cd .. && yarn install)` to install in the parent directory
+2. `yarn install` here
+3. `react-native run-android` or `react-native run-ios`
+
+You can also use `npm` instead of `yarn` if you prefer.
---
+++ src/__tests__/index.test.js
@@ -0,0 +1,15 @@
+import 'react-native';
+import React from 'react';
+
+// Note: test renderer must be required after react-native.
+import renderer from 'react-test-renderer';
+
+import App from '../App';
+
+describe('Snackbar example app', () => {
+  it('renders without crashing', () => {
+    renderer.create(
+      <App />,
+    );
+  });
+});
